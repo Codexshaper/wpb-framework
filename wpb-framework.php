35ce63e24227047c208d95962327ee66285a1165
Md Abu Ahsan Basir <maab.career@gmail.com>
2020-06-19 11:04:03 +0600

    Updated Rename files and classes name

--- wpb-framework.php
+++ wpb-framework.php
@@ -21,7 +21,7 @@
  * Author URI:        https://github.com/maab16
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       wp-plugin-builder
+ * Text Domain:       wpb-framework
  * Domain Path:       /languages
  */
 
@@ -48,30 +48,30 @@ define( 'WPB_FRAMEWORK_VERSION', '1.0.0' );
 
 /**
  * The code that runs during plugin activation.
- * This action is documented in includes/class-wp-plugin-builder-activator.php
+ * This action is documented in includes/class-wpb-framework-activator.php
  */
-function activate_wp_plugin_builder() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp-plugin-builder-activator.php';
-	Wp_Plugin_Builder_Activator::activate();
+function activate_wpb_framework() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wpb-framework-activator.php';
+	WPB_Framework_Activator::activate();
 }
 
 /**
  * The code that runs during plugin deactivation.
- * This action is documented in includes/class-wp-plugin-builder-deactivator.php
+ * This action is documented in includes/class-wpb-framework-deactivator.php
  */
-function deactivate_wp_plugin_builder() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp-plugin-builder-deactivator.php';
-	Wp_Plugin_Builder_Deactivator::deactivate();
+function deactivate_wpb_framework() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wpb-framework-deactivator.php';
+	WPB_Framework_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_wp_plugin_builder' );
-register_deactivation_hook( __FILE__, 'deactivate_wp_plugin_builder' );
+register_activation_hook( __FILE__, 'activate_wpb_framework' );
+register_deactivation_hook( __FILE__, 'deactivate_wpb_framework' );
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-wp-plugin-builder.php';
+require plugin_dir_path( __FILE__ ) . 'includes/class-wpb-framework.php';
 
 /**
  * Begins execution of the plugin.
