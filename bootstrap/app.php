20911f13ec71cad04a2eeadb3495c6f02003db95
Md Abu Ahsan Basir <maab.career@gmail.com>
2020-06-23 14:41:05 +0600

    Updated global app name

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,33 +2,22 @@
 require_once(ABSPATH.'wp-includes/pluggable.php');
 
 require_once __DIR__.'/../vendor/autoload.php';
-// require_once __DIR__.'/../routes/web.php';
-
-require_once __DIR__. '/../vendor/illuminate/support/helpers.php';
-require_once __DIR__. '/../vendor/codexshaper/wpb-foundation/src/helpers.php';
-require_once __DIR__. '/../vendor/codexshaper/wpb-foundation/src/shortcodes.php';
 
 use CodexShaper\WP\Application;
-use Illuminate\Support\Str;
 
-$wpb = $app = (new Application([
+$app = $container = (new Application([
 	'paths' => [
 		'root' => WPB_APP_ROOT
 	]
 ]))->getInstance();
 
-// require_once PLUGIN_BASE_PATH . '../database/migrations/2014_10_12_000000_create_custom_options_table.php';
+global $app;
 
-// with(new CreateCustomOptionsTable)->up();
-
-
-global $wpb;
-
-$app->singleton(
+$container->singleton(
     Illuminate\Contracts\Http\Kernel::class,
     CodexShaper\App\Http\Kernel::class
 );
-$app->singleton(
+$container->singleton(
     \Illuminate\Contracts\Debug\ExceptionHandler::class,
     \CodexShaper\App\Exceptions\Handler::class
 );
@@ -36,7 +25,7 @@ $app->singleton(
 if(\CodexShaper\WP\Support\Facades\Route::exists(\Illuminate\Http\Request::capture())) {
 	try {
 
-		$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+		$kernel = $container->make(Illuminate\Contracts\Http\Kernel::class);
 
 		$response = $kernel->handle(
 		    $request = Illuminate\Http\Request::capture()
